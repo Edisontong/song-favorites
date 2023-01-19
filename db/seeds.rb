19be791629c6a3ed42e88d2fecb9beb07fd0543f
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-01-19 13:09:44 -0700

    added songs to the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+song = Song.new(title: "Pour some sugar on me", album: "Hysteria", artist: "Def Leppad", year: 1987)
+song.save
+
+song = Song.new(title: "Blackbird", album: "The White Album", artist: "The Beattles", year: 1968)
+song.save
+
+song = Song.new(title: "She's always a woman", album: "The Stranger", artist: "Billy Joel", year: 1977)
+song.save
+
+song = Song.new(title: "Soldier, Poet, King", album: "Dear Wormwood", artist: "The Oh Hellos", year: 2015)
+song.save
